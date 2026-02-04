85bb256622458c0220a47331b33b8c0c7baca209
Bruno Esteves <92205681+GitLuci@users.noreply.github.com>
2026-02-04 00:00:38 +0000

    Add timebucket assets download and UI
    Introduce downloading of timeline bucket assets and surface it in the home UI.\n\n- Add load_screen_data/data_actions.py: new timebucket_assets_download() to fetch a timeline bucket id, retrieve archive info and return archived asset bytes via immich_integration.\n- Update load_screen_data/data_loader.py: add timebucket_assets_info() to report total size (MB) and archive count for a given time bucket.\n- Update screens/home.py: manage session state for current bucket and its info, wire a selectbox to update those states, display selection and info, and add a "Prepare download" button that exposes a download_button to fetch the zip via data_actions.timebucket_assets_download().\n\nThese changes enable selecting a timeline bucket in the UI and downloading its archived assets as a zip.

--- screens/home.py
+++ screens/home.py
@@ -1,19 +1,43 @@
 import streamlit as st
+from click import option
+
 from load_screen_data import data_loader
+from load_screen_data import data_actions
 
 if 'refreshed-buckets' not in st.session_state:
     st.session_state['refreshed-buckets'] = None
-
+if 'current-bucket-info' not in st.session_state:
+    st.session_state['current-bucket-info'] = None
+if 'current-bucket' not in st.session_state:
+    st.session_state['current-bucket'] = None
 
 if st.session_state['refreshed-buckets'] == None:
     st.session_state['refreshed-buckets'] = data_loader.refreshed_buckets()
 
+def option_on_change(curr_option):
+    if curr_option != None:
+        st.session_state['current-bucket-info'] = data_loader.timebucket_assets_info(curr_option["timeBucket"])
+        st.session_state['current-bucket'] = curr_option
 
-
-
-option = st.selectbox(
+option_on_change(st.selectbox(
     "What Time Line Bucket you want download ?",
-    (st.session_state['refreshed-buckets']),
-)
+    (st.session_state['refreshed-buckets']),index=None
+))
+
+
 st.write("Count is the number os items (photos or videos) in determinate time bucket")
-st.write("You selected option:", option)
+st.write("You selected option:", st.session_state['current-bucket'])
+st.write("Had:", st.session_state['current-bucket-info'])
+
+if st.session_state['current-bucket'] != None:
+    if st.button("Prepare download"):
+        st.download_button(
+            label="Download Zip",
+            data=data_actions.timebucket_assets_download(st.session_state['current-bucket']['timeBucket']),
+            file_name="images.zip",
+            mime="bytes/zip",
+            icon=":material/download:",
+        )
+    st.write('For huge amount of data, the button can have a delay to show, wait for some time and the download button will be appear.')
+
+
